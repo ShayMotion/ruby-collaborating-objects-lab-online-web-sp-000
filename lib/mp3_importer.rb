58daa6272e9d097ee697634a8306b87a0e1a21bf
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-08-29 01:53:23 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,21 @@
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    @files = []
+  file = Dir.entries(@path)
+    file.each do |file|
+    end
+    
+  end
+
+  def import
+    self.files.each do |filename|
+      Song.new_by_filename(filename)
+    end
+  end
+end
