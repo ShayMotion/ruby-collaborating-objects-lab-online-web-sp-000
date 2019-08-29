f4b1e8cc7da221b6df2a511c05862aee10f3c51d
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-08-29 02:22:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,45 @@
-class Song
-  attr_accessor :name, :artist
+class Artist
+  attr_accessor :name, :songs
 
-  def initialize(name)
+   @@all = []
+
+   def initialize(name)
     @name = name
+    @songs = []
+    save
   end
 
-
-  def artist_name=(name_string)
-      song_artist = Artist.find_or_create_by_name(name_string)
-       @artist = song_artist
-      @artist.add_song(self)
+   def self.all
+    @@all
   end
+
+   def self.create(name)
+    artist = self.new(name)
+    artist.name = name
+    artist
+  end
+
+   def self.find(name)
+    self.all.detect { |artist| artist.name == name }
+  end
+
+   def self.find_or_create_by_name(name)
+    self.find(name) || self.create(name)
+  end
+
+   def add_song(song)
+    @songs << song
+  end
+
+   def print_songs
+    songs.each { |song| puts song.name }
+  end
+
+   def save
+    @@all << self
+  end
+
+   def songs
+    @songs
+  end
+end 
